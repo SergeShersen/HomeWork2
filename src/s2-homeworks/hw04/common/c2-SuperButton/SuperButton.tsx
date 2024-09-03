96f8f6368e9623e669bf6d13de7598e4543d4ba8
SergeShershen <serge_qw@mail.ru>
2024-09-03 22:07:49 +0300

    first

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -32,12 +32,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-         +' ' + (disabled ?  s.disabled :
-            xType === 'red' ?s.red:  
-            xType === 'secondary'? s.secondary 
-             : s.default)
-        //         : xType === 'red'
-        //     ? ...
+    + ' ' + (disabled ? s.disabled :
+        xType === 'red' ? s.red :
+        xType === 'secondary' ? s.secondary :
+        s.default
+    ) 
+   
+
         
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -15,18 +15,22 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
+
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+               
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+               
+                <Route path='/' element={<Navigate to = {'/pre-junior'}/>} />
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path='/pre-junior' element={<PreJunior/>} />
+                <Route path='/junior' element={<Junior/>} />
+                <Route path='/junior-plus' element={<JuniorPlus/>} />
+
+                <Route path='/error404.tsx' element={<Error404/>}/>
+                <Route path='/*' element={<Navigate to = {'/error404.tsx'}/>}/>
             </Routes>
         </div>
     )
