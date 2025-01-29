779db2c7bdd2a715c56509f822a1074fbb42859f
SergeShershen <serge_qw@mail.ru>
2025-01-29 16:53:55 +0300

    first

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {initStateType, loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -15,14 +15,19 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, any>(store => store.loading.isLoading)
+   
+    const dispatch = useDispatch()
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
-        // setTimeout
+        dispatch(loadingAC(true))
+        
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        },1500)
     }
-
+    console.log(isLoading)
     return (
         <div id={'hw10'}>
             <div className={s2.hwTitle}>Homework #10</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,21 @@
-const initState = {
+export type  initStateType = {
+    isLoading:boolean
+}
+
+
+const initState: initStateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING' : {
+            return {...state,
+                isLoading: action.isLoading}
+            
+        }
+
 
         default:
             return state
